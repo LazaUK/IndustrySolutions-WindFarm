6a7aeb2f6ca11905ab14d1459e8cf611d1af2725
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2022-08-23 10:07:47 +0100

    Add files via upload

--- Python_sample/IoT_Telemetry_single.py
+++ Python_sample/IoT_Telemetry_single.py
@@ -1,1 +1,25 @@
+import asyncio
+from azure.iot.device.aio import IoTHubDeviceClient
 
+
+async def main():
+    # Fetch the connection string from an enviornment variable
+    conn_str = "HostName=iotHubWindFarm.azure-devices.net;DeviceId=lazizSimulator;SharedAccessKey=rtSo1imlttwffxnBA3NQE7aQ/peXmDVb2FnZFPacPuo="
+
+    # Create instance of the device client using the authentication provider
+    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+
+    # Connect the device client.
+    await device_client.connect()
+
+    # Send a single message
+    print("Sending message...")
+    await device_client.send_message("This is a message that is being sent")
+    print("Message successfully sent!")
+
+    # finally, shut down the client
+    await device_client.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
