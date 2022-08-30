bad851a1ddecaa1a4d650c9435e81bd956bc452c
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2022-08-30 16:42:53 +0100

    Add files via upload

--- Helium_Function/Helium_Decoder.js
+++ Helium_Function/Helium_Decoder.js
@@ -1,1 +1,20 @@
-
+function Decoder(bytes, port) {
+ 
+    var decoded = {};
+ 
+    function transformers(bytes){
+        value = bytes[0] * 256 + bytes[1];
+        if (value >= 32768) {
+        value = 32768 - value;
+        }
+        value = value/100.0;
+        return value;
+    }
+ 
+    if (port == 8) {
+        decoded.generator_temperature = transformers(bytes.slice(0, 2));
+        decoded.generator_humidity = transformers(bytes.slice(2, 4));
+    }
+ 
+    return decoded;
+}
