08cce87ecd30b5c13bf1a65c888493a1e91fa7f4
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2022-09-01 16:53:38 +0100

    Add files via upload

--- Azure_Function/IoTHubtoTwins.cs
+++ Azure_Function/IoTHubtoTwins.cs
@@ -1,1 +1,61 @@
+using System;
+using Azure;
+using System.Net.Http;
+using Azure.Core.Pipeline;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Azure.Messaging.EventGrid;
 
+namespace IotHubtoTwins
+{
+    public class IoTHubtoTwins
+    {
+        private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        [FunctionName("IoTHubtoTwins")]
+        // While async void should be used with caution, it's not uncommon for Azure function, since the function isn't awaiting the task.
+        #pragma warning disable AZF0001 // Suppress async void error
+        public async void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        #pragma warning restore AZF0001 // Suppress async void error
+        {
+            if (adtInstanceUrl == null) log.LogError("Application setting \"ADT_SERVICE_URL\" not set");
+
+            try
+            {
+                // Authenticate with Digital Twins
+                var cred = new DefaultAzureCredential();
+                var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
+                log.LogInformation($"ADT service client connection created.");
+            
+                if (eventGridEvent != null && eventGridEvent.Data != null)
+                {
+                    log.LogInformation(eventGridEvent.Data.ToString());
+
+                    // <Find_device_ID_and_temperature>
+                    JObject deviceMessage = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+                    string deviceId = (string)deviceMessage["systemProperties"]["iothub-connection-device-id"];
+                    var temperature = deviceMessage["body"]["generator_temperature"];
+                    // </Find_device_ID_and_temperature>
+
+                    log.LogInformation($"Device:{deviceId} Temperature is:{temperature}");
+
+                    // <Update_twin_with_device_temperature>
+                    var updateTwinData = new JsonPatchDocument();
+                    updateTwinData.AppendReplace("/Temperature", temperature.Value<double>());
+                    await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
+                    // </Update_twin_with_device_temperature>
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Error in ingest function: {ex.Message}");
+            }
+        }
+    }
+}
